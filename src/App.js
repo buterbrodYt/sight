b5e435c78bc8d3ab6b5da7f15f0ec3b512c03de9
buterbrodYt <butterbrodyt@gmail.com>
2025-01-23 17:38:12 +0300

    configured url path and build

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export const queryClient = new QueryClient();
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <HashRouter basename="/sight">
+      <HashRouter>
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
